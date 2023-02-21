2a064912895febeba663559c620b7454f8a1bea1
Deemou <4106243@naver.com>
2023-02-21 23:33:46 +0900

    fix(prisma): issue - Already 10 Prisma Clients are actively running.

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,11 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var db: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const client = new PrismaClient();
 
-if (process.env.NODE_ENV === "development") global.db = client;
+if (process.env.NODE_ENV === "development") globalForPrisma.prisma = client;
 
 export default client;
