30bd361bda7a8ff7a7188b78ceaaaa1781db501d
Deemou <4106243@naver.com>
2023-02-21 19:49:32 +0900

    feat(libs): add withHandler

---
+++ libs/server/withHandler.ts
@@ -0,0 +1,39 @@
+import { NextApiHandler } from "next";
+
+export interface ResponseType {
+  ok: boolean;
+  [key: string]: any;
+}
+
+type method = "GET" | "POST" | "DELETE";
+
+interface ConfigType {
+  methods: method[];
+  handler: NextApiHandler;
+  isPrivate?: boolean;
+}
+
+export default function withHandler({
+  methods,
+  handler,
+  isPrivate = true,
+}: ConfigType): NextApiHandler {
+  return async (req, res) => {
+    if (req.method && !methods.includes(req.method as any)) {
+      return res.status(405).end();
+    }
+    if (isPrivate && !req.session.user) {
+      return res.status(401).json({ ok: false, error: "Plz log in." });
+    }
+    try {
+      await handler(req, res);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof Error) {
+        res.status(500).json({ error: error.message });
+      } else {
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    }
+  };
+}
